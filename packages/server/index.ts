bd9539dc659851e9e6d85e99f032e1c40fdc87ef
Sansita Jain <sansita7406@gmail.com>
2026-06-21 18:53:21 +0530

    manage API key

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import type { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Define a route
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.send(process.env.GEMINI_API_KEY);
 });
 
 // Start the server
