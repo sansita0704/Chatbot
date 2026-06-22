78af39d2625b0a9ec497e0f7c99fe145b8d46c2b
Sansita Jain <sansita7406@gmail.com>
2026-06-22 15:41:33 +0530

    build the chat API

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { GoogleGenAI } from '@google/genai';
 
 dotenv.config();
 
+const client = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+});
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 // Define a route
@@ -16,6 +22,24 @@ app.get('/api/hello', (req: Request, res: Response) => {
     res.json({
         message: 'Hello World!',
     });
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+    // 1. Take user's prompt from the chat
+    const { prompt } = req.body;
+
+    // 2. Send prompt to gemini
+    const response = await client.models.generateContent({
+        model: 'gemini-2.5-flash',
+        contents: prompt,
+        config: {
+            temperature: 0.2,
+            maxOutputTokens: 100,
+        },
+    });
+
+    // 3. Return the JSON object
+    res.json({ message: response.text });
 });
 
 // Start the server
