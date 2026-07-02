be559925ec9e2bc55fe807423c5a20ce576edda0
Sansita Jain <sansita7406@gmail.com>
2026-07-02 15:59:15 +0530

    display reviews

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,10 @@
 import ChatBot from './components/chat/ChatBot';
+import ReviewList from './components/reviews/ReviewList';
 
 function App() {
     return (
         <div className="p-4 h-screen w-2xl mx-auto">
+            <ReviewList productId={1} />
             <ChatBot />
         </div>
     );
